a7165447acaa0dc7236e5c845d6e84a9e8b49e31
Cody Fleenor <35227631+wizard-dev-jack@users.noreply.github.com>
2023-01-05 21:20:25 -0800

    Adding amplify to application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "portfoliowebsite17fff1f9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
